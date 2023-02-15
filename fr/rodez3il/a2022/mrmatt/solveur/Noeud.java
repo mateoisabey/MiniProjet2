bf3c27542546c01eddf2f0d0e9b76f92aa4e6eea
mateoisabey <mateoisabey@gmail.com>
2023-02-15 19:01:58 +0100

    update de la methode calculerFils

--- fr/rodez3il/a2022/mrmatt/solveur/Noeud.java
+++ fr/rodez3il/a2022/mrmatt/solveur/Noeud.java
@@ -2,13 +2,17 @@ package fr.rodez3il.a2022.mrmatt.solveur;
 
 import fr.rodez3il.a2022.mrmatt.solveur.structures.DictionnaireChaine;
 import fr.rodez3il.a2022.mrmatt.solveur.structures.ListeTableau;
+import fr.rodez3il.a2022.mrmatt.sources.Commande;
+import fr.rodez3il.a2022.mrmatt.sources.Niveau;
+
+import java.util.Objects;
 
 public class Noeud {
 
-    private DictionnaireChaine<String, Noeud> configPossible;
+    private static DictionnaireChaine<String, Noeud> configPossible;
 
 
-    private Noeud state;
+    private static Niveau state;
 
     //fils ca sera un tableau de noeud 4 position (haut, bas, Gauche, Droite)
     private ListeTableau<Noeud> childs ;
@@ -16,17 +20,128 @@ public class Noeud {
     private String cmd;
 
     private String res = null ;
-    private String nextCmd;
+    private String listeCmd;
 
-    private Boolean visited;
+    private static Boolean visited;
 
-    public Noeud(DictionnaireChaine<String, Noeud> configPossible, Noeud state, String cmd, String res, String nextCmd) {
+    /**
+     * Constructeur d'un noeud.
+     * @param configPossible
+     * @param state
+     * @param cmd
+     * @param res
+     * @param listeCmd
+     */
+    public Noeud(DictionnaireChaine<String, Noeud> configPossible, Niveau state, String cmd, String res, String listeCmd) {
         this.configPossible = configPossible;
         this.state = state;
         this.childs = new ListeTableau<>();
         this.cmd = cmd;
         this.res = res;
-        this.nextCmd = nextCmd;
+        this.listeCmd = listeCmd;
         this.visited = false;
+    }
+
+
+    public String calculerFils(){
+        visited = true;
+        configPossible.inserer(state.valeurChaine(),this);
+        Commande[] commandesPossible = Commande.values();
+        //boucler pour toutes les directions
+        for (int i = 0; i < commandesPossible.length; i++) {
+            //Verification si c'est en cours
+            if (state.enCours()) {
+                //verification si le joueur peur se déplacer
+                if (state.deplacementPossible(commandesPossible[i])) {
+                    listeCmd = this.cmd + "" + commandesPossible[i];
+                    Niveau newState = state.copier();
+                    newState.calcule();
+                    Boolean aGagne = newState.estGagnant();
+                    affichageGagnant(aGagne);
+
+                }
+            }
+        }
+
+        return null;
+    }
+    public String affichageGagnant(Boolean aGagne, Niveau newState) {
+        String etatNoeud;
+        if (aGagne) {
+            res = listeCmd;
+        } else {
+            Noeud noeud;
+            etatNoeud = newState.valeurChaine();
+        }
+    }
+
+
+    public DictionnaireChaine<String, Noeud> getConfigPossible() {
+        return configPossible;
+    }
+
+    public void setConfigPossible(DictionnaireChaine<String, Noeud> configPossible) {
+        this.configPossible = configPossible;
+    }
+
+    public Niveau getState() {
+        return state;
+    }
+
+    public void setState(Niveau state) {
+        this.state = state;
+    }
+
+    public ListeTableau<Noeud> getChilds() {
+        return childs;
+    }
+
+    public void setChilds(ListeTableau<Noeud> childs) {
+        this.childs = childs;
+    }
+
+    public String getCmd() {
+        return cmd;
+    }
+
+    public void setCmd(String cmd) {
+        this.cmd = cmd;
+    }
+
+    public String getRes() {
+        return res;
+    }
+
+    public void setRes(String res) {
+        this.res = res;
+    }
+
+    public String getListeCmd() {
+        return listeCmd;
+    }
+
+    public void setListeCmd(String listeCmd) {
+        this.listeCmd = listeCmd;
+    }
+
+    public Boolean getVisited() {
+        return visited;
+    }
+
+    public void setVisited(Boolean visited) {
+        this.visited = visited;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Noeud noeud = (Noeud) o;
+        return Objects.equals(configPossible, noeud.configPossible) && Objects.equals(state, noeud.state) && Objects.equals(childs, noeud.childs) && Objects.equals(cmd, noeud.cmd) && Objects.equals(res, noeud.res) && Objects.equals(listeCmd, noeud.listeCmd) && Objects.equals(visited, noeud.visited);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(configPossible, state, childs, cmd, res, listeCmd, visited);
     }
 }
